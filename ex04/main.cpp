87b51e49967ed31b155b67b50b11f84fb9b165a6
Afonso Coutinho <116525378+afonsopc@users.noreply.github.com>
2025-04-09 00:56:52 +0100

    saved

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,31 +6,19 @@
 /*   By: afpachec <afpachec@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/08 23:59:43 by afpachec          #+#    #+#             */
-/*   Updated: 2025/04/09 00:35:28 by afpachec         ###   ########.fr       */
+/*   Updated: 2025/04/09 00:54:47 by afpachec         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <fcntl.h>
-#include <unistd.h>
+#include <fstream>
+#include <sstream>
+#include <string>
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 1024
-#endif
-
-std::string	read_file(int fd) {
-	char	buff[BUFFER_SIZE + 1] = {0};
-	std::string	contents;
-	ssize_t	read_bytes;
-
-	read_bytes = read(fd, buff, BUFFER_SIZE);
-	while (read_bytes > 0)
-	{
-		buff[read_bytes] = '\0';
-		contents += buff;
-		read_bytes = read(fd, buff, BUFFER_SIZE);
-	}
-	return (contents);
+std::string	read_file(std::ifstream &file) {
+	std::stringstream	buff;
+	buff << file.rdbuf();
+	return buff.str();
 }
 
 std::string	replace_instances(std::string master, std::string to_replace, std::string replace_with) {
@@ -48,9 +36,9 @@ std::string	replace_instances(std::string master, std::string to_replace, std::s
 }
 
 void	write_to_file(std::string file_name, std::string contents) {
-	int		fd = open(file_name.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	write(fd, contents.c_str(), contents.length());
-	close(fd);
+	std::ofstream	file(file_name.c_str());
+	if (file.is_open())
+		file << contents;
 }
 
 int	main(int argc, char **argv) {
@@ -59,14 +47,13 @@ int	main(int argc, char **argv) {
 		std::cout << "Invalid arguments (better yourself loser)" << std::endl;
 		return (1);	
 	}
-	int	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
+	std::ifstream	file(argv[1]);
+	if (!file.is_open())
 	{
 		std::cout << "File not found" << std::endl;
 		return (1);
 	}
-	std::string contents = read_file(fd);
-	close(fd);
+	std::string contents = read_file(file);
 	std::string	replaced_contents = replace_instances(contents, std::string(argv[2]), std::string(argv[3]));
 	write_to_file(std::string(argv[1]) + ".replace", replaced_contents);
 }
